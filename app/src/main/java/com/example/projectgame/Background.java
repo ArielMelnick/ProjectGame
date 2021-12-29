cdb6ac40f633f9b225b12794cd46826ff75f2164
student <ariellmelnick@gmail.com>
2021-12-29 14:32:10 +0200

    background update

--- app/src/main/java/com/example/projectgame/Background.java
+++ app/src/main/java/com/example/projectgame/Background.java
@@ -11,7 +11,7 @@ public class Background {
 
      public Background (int screenX, int screenY, Resources res) { // res contains the image data
          background = BitmapFactory.decodeResource(res, R.drawable.background); // bitmapFactory creates bitmaps
-         Bitmap.createScaledBitmap(background, screenX, screenY, false);
+         background = Bitmap.createScaledBitmap(background, screenX, screenY, false);
 
      }
 }
