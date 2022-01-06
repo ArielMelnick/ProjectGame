810c414862304ad235cba799e427f33eda7f24bb
student <ariellmelnick@gmail.com>
2022-01-06 17:41:28 +0200

    Some more comments/explanations

--- app/src/main/java/com/example/projectgame/MainActivity.java
+++ app/src/main/java/com/example/projectgame/MainActivity.java
@@ -19,7 +19,7 @@ public class MainActivity extends AppCompatActivity {
 
             @Override
             public void onClick(View v) {
-                startActivity(new Intent(MainActivity.this, GameActivity.class)); // to show GameView
+                startActivity(new Intent(MainActivity.this, GameActivity.class)); // to start gameActivity and through that to show GameView (like MainActivity and activity_main)
 
             }
         });
