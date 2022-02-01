00c6c402038182ced678826bfbb28c328338d44c
User <ariellmelnick@gmail.com>
2022-02-01 20:19:26 +0200

    I  added the option to call me

--- app/src/main/java/com/example/projectgame/MainActivity.java
+++ app/src/main/java/com/example/projectgame/MainActivity.java
@@ -1,15 +1,22 @@
 package com.example.projectgame;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 
+import android.Manifest;
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.WindowManager;
 import android.widget.PopupMenu;
 import android.widget.TextView;
+
+import java.net.URI;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -17,10 +24,6 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
-        
-
-
         setContentView(R.layout.activity_main);
 
 
@@ -29,19 +32,29 @@ public class MainActivity extends AppCompatActivity {
 
             @Override
             public void onClick(View v) {
+
                 startActivity(new Intent(MainActivity.this, GameActivity.class)); // to start gameActivity and through that to show GameView (like MainActivity and activity_main)
 
             }
-
         });
-
 
     }
 
     public boolean onCreateOptionsMenu(Menu menu) {
-        super.onCreateOptionsMenu(menu);
         getMenuInflater().inflate(R.menu.menu, menu);
         return  true;
 
     }
+
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+
+        if(id == R.id.Contact){
+            Intent intent = new Intent(Intent.ACTION_DIAL, Uri.parse("tel:"+"0546306568"));
+            startActivity(intent);
+        }
+        return true;
+
+    }
+
 }
