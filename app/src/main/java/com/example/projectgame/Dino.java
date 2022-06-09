fa7dc53b3824d7d1f4b4ae1e151c25cb6137293e
User <ariellmelnick@gmail.com>
2022-06-09 15:51:00 +0300

    I organized in Robot and in Dino the building function in a couple of functions. and some other little things

--- app/src/main/java/com/example/projectgame/Dino.java
+++ app/src/main/java/com/example/projectgame/Dino.java
@@ -8,13 +8,26 @@ import android.graphics.Rect;
 public class Dino {
 
     int x, y; // the coordinates of the dino on the screen
-    private int counter = 1;
     int width, height;
+    private int counter = 1;
     int speed = 20;
     Bitmap dino1, dino2, dino3, dino4, dino5, dino6, dino7, dino8;  // the stages of the running dino
     boolean wasShot = true; // to check whether the dino got shot(and thus got out of the screen) or he got out of the screen without getting shot
 
     Dino(Resources res) {
+
+        decodeImages(res);
+
+        setWidthAndHeight();
+
+        createScaledBitmaps();
+
+        x = 0;
+        y = -height;
+
+    }
+
+    public void decodeImages(Resources res) {
 
         dino1 = BitmapFactory.decodeResource(res, R.drawable.dino_run_1);
         dino2 = BitmapFactory.decodeResource(res, R.drawable.dino_run_2);
@@ -25,6 +38,10 @@ public class Dino {
         dino7 = BitmapFactory.decodeResource(res, R.drawable.dino_run_7);
         dino8 = BitmapFactory.decodeResource(res, R.drawable.dino_run_8);
 
+    }
+
+    public void setWidthAndHeight() {
+
         width = dino1.getWidth();
         height = dino1.getHeight();
 
@@ -34,6 +51,10 @@ public class Dino {
         width *= GroundGameView.screenRatioX;
         height *= GroundGameView.screenRatioY;
 
+    }
+
+    public void createScaledBitmaps() {
+
         dino1 = Bitmap.createScaledBitmap(dino1, width, height, false);
         dino2 = Bitmap.createScaledBitmap(dino2, width, height, false);
         dino3 = Bitmap.createScaledBitmap(dino3, width, height, false);
@@ -42,9 +63,6 @@ public class Dino {
         dino6 = Bitmap.createScaledBitmap(dino6, width, height, false);
         dino7 = Bitmap.createScaledBitmap(dino7, width, height, false);
         dino8 = Bitmap.createScaledBitmap(dino8, width, height, false);
-
-        x = 0;
-        y = -height;
 
     }
 
